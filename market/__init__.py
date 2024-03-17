ee61dae245442afa725a316d2ed6355f9d46da0c
ArianSanzo <113949284+ArianSanzo@users.noreply.github.com>
2024-03-16 22:40:11 -0300

    User Authentication and Login

--- market/__init__.py
+++ market/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '02bfe29a6394481e0be8df13'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -18,8 +18,14 @@ class RegisterForm(FlaskForm):
         if email_address:
             raise ValidationError('Email address already exist.')
 
-    username = StringField(label='Username: ', validators=(Length(min=3, max=30), DataRequired()))
-    email_address = StringField(label='Email address: ', validators=(Length(max=50), Email(), DataRequired()))
-    password1 = PasswordField(label='Password: ', validators=(Length(min=6, max=60), DataRequired()))
-    password2 = PasswordField(label='Confirm password: ', validators=((EqualTo('password1')), DataRequired()))
+    username = StringField(label='Username: ', validators=[Length(min=3, max=30), DataRequired()])
+    email_address = StringField(label='Email address: ', validators=[Length(max=50), Email(), DataRequired()])
+    password1 = PasswordField(label='Password: ', validators=[Length(min=6, max=60), DataRequired()])
+    password2 = PasswordField(label='Confirm password: ', validators=[(EqualTo('password1')), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Username: ', validators=[DataRequired()])
+    password = PasswordField(label='Password: ', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
